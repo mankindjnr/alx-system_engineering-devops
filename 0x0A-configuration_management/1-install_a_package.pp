68d12a638a1c6bd438ffb4b5b5636e0f9145fba7
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-03-24 00:50:26 -0700

    using puppet to install flask from pip3

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,9 @@
+#using puppet to install flask from pip3
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  creates => '/usr/local/bin/flask',
+}
