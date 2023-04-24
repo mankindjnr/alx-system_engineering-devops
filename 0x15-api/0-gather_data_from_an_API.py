37194f796fd6b4ca42fffd763f7a6d808335aafd
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-04-24 04:41:38 -0700

    datagathering with api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -15,13 +15,9 @@ if __name__ == "__main__":
     user_url = f"https://jsonplaceholder.typicode.com/users/{param}"
     task_url = f"https://jsonplaceholder.typicode.com/todos/"
 
-    # calling a requests.get request t0 tasks and users which
-    # will respond witht the item with id 1
     user_response = requests.get(user_url)
     task_response = requests.get(task_url)
 
-    # call json on the response object to view the
-    # data that is sent back by the API
     user_resp = user_response.json()
     task_resp = task_response.json()
 
@@ -35,10 +31,8 @@ if __name__ == "__main__":
             tasks += 1
             if my_dicts.get("completed") is True:
                 completed_tasks += 1
-                # print()
-                # print(my_dicts)
 
-                name = user_resp["name"]
+    name = user_resp["name"]
     print(f"Employee {name} is done with tasks({completed_tasks}/{tasks}):")
 
     # looping a list of and printing only the tasks done
